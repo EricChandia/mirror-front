e6291be9fd86a3d8e328946eca7db23a03fba4b4
EricChandia <eric.chandia.ti@gmail.com>
2022-10-10 03:44:10 -0300

    fix uploadPhotos layout

--- src/pages/profile/CreateProfile.js
+++ src/pages/profile/CreateProfile.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 import { lightColor0, lightColor1, lightColor2, lightColor3, lightColor4 } from "../../assets/globalStyles/colors";
 import { useNavigate } from "react-router-dom";
-import { TransparentInput1 } from "../../components/Input";
-import { useState } from "react";
+import { TransparentInput1, TransparentTextArea } from "../../components/Input";
+import { useEffect, useState } from "react";
 import { IconLeftButton, IconRightButton } from "../../components/Icon";
 import { LightButton1 } from "../../components/Button";
 import * as service from "../../services/profileService";
 import getHeaderToken from "../../utils/getHeaderToken";
-
+import * as profileService from "../../services/profileService";
 
 export default function CreateProfile(){
     const navigate = useNavigate();
@@ -17,19 +17,40 @@ export default function CreateProfile(){
     const [age, setAge] = useState("");
     const [occupation, setOccupation] = useState("");
     const [schooling, setSchooling] = useState("");
+    const [description, setDescription] = useState("");
+    const [gender, setGender] = useState("");
+    const [identification, setIdentification] = useState("");
+    const [lookingFor, setLookingFor] = useState("");
+    const config = getHeaderToken();
+
+    useEffect(() => {
+        async function checkIfUserHasProfile(){
+            const userProfile = await profileService.getUserProfile(config);
+            if(userProfile){
+                navigate("/home");
+            }
+        }
+
+        checkIfUserHasProfile();
+    }, [])
 
     async function saveProfile(){
         const config = getHeaderToken();
 
         const data = {
             name,
+            age,
             occupation,
-            schooling
+            schooling,
+            description,
+            gender,
+            identification,
+            lookingFor
         }
         
         try {
             await service.createProfile(data, config);
-            navigate("/home");
+            navigate("/uploadPhotos");
         }catch(error){
             alert("Ops, ocorreu um erro, tente novamente mais tarde");
             console.log(error);
@@ -37,18 +58,66 @@ export default function CreateProfile(){
 
     }
 
+    function nextPage(){
+        if(page === 0 && name.length < 3){
+            alert("O nome é obrigatório :(");
+            return;
+        }
+
+        if(page === 2 && gender.length === 0){
+            alert("Essa informação é obrigatória :(");
+            return;
+        }
+
+        if(page === 3 && lookingFor.length === 0){
+            alert("Essa informação é obrigatória :(");
+            return;
+        }
+        
+        setPage(page+1);
+    }
+
+    function genderChoice(e){
+        setGender(e.target.value);
+    }
 
     return(
         <CreateProfileScreen>
             {page === 0 ? <TransparentInput1 placeholder="Seu lindo nome aqui" height="6%" width="60%" value={name} onChange={(e) => setName(e.target.value)}/> : false}
+            {page === 1 ? <h1>Aqui nada é obrigatório ok? Sinta-se à vontade de preencher o que preferir.</h1> : false}
             {page === 1 ? <TransparentInput1 placeholder="Idade" type="number" height="6%" width="60%" value={age} onChange={(e) => setAge(e.target.value)}/> : false}
-            {page === 2 ? <TransparentInput1 placeholder="Profissão" height="6%" width="60%" value={occupation} onChange={(e) => setOccupation(e.target.value)}/> : false}
-            {page === 2 ? <TransparentInput1 placeholder="Escolaridade" height="6%" width="60%" value={schooling} onChange={(e) => setSchooling(e.target.value)}/> : false}
+            {page === 1 ? <TransparentInput1 placeholder="Profissão" height="6%" width="60%" value={occupation} onChange={(e) => setOccupation(e.target.value)}/> : false}
+            {page === 1 ? <TransparentInput1 placeholder="Escolaridade" height="6%" width="60%" value={schooling} onChange={(e) => setSchooling(e.target.value)}/> : false}
+            {page === 1 ? <TransparentTextArea placeholder="Escreva uma linda descrição sobre você aqui" height="15%" width="60%" value={description} onChange={(e) => setDescription(e.target.value)}/> : false}
 
-            {page > 2 ? false : <div onClick={() => {setPage(page+1)}}><IconRightButton /></div> }
+            {page === 2 ? 
+            <>
+                <h1>Escolha a opção que te descreve melhor, em seguida, você pode falar mais sobre isso.</h1> 
+                <GenderChoice onChange={genderChoice}>
+                <input type="radio" value="Man" name="gender" /> Homem 
+                <input type="radio" value="Woman" name="gender" /> Mulher
+                <input type="radio" value="Any" name="gender" /> Não binário
+                </GenderChoice>
+                <TransparentTextArea placeholder="Você se identifica como? Ex: Hetero, Trans, Não binário, Cis etc" fontSize="18px" height="10%" width="80%" value={identification} onChange={(e) => setIdentification(e.target.value)}/>
+            </>
+            : false}
+            
+            {page === 3 ? <h1>Você está em busca de um amor que se identifica como?</h1> : false}
+            
+            {page === 3 ? 
+                
+                <GenderChoice onChange={(e) => setLookingFor(e.target.value)}>
+                    <input type="radio" value="Man" name="gender" /> Homem 
+                    <input type="radio" value="Woman" name="gender" /> Mulher
+                    <input type="radio" value="Any" name="gender" /> Tanto faz
+                </GenderChoice>
+                                
+            : false}
+
+            {page > 3 ? false : <div onClick={() => nextPage()}><IconRightButton /></div> }
             {page > 0 ? <div onClick={() => {setPage(page-1)}}><IconLeftButton /></div> : false}
 
-            {page === 3 ? <SaveProfileBtn height="20%" onClick={() => saveProfile()}>Tudo certo</SaveProfileBtn> : false}
+            {page === 4 ? <SaveProfileBtn height="20%" onClick={() => saveProfile()}>Tudo certo</SaveProfileBtn> : false}
         </CreateProfileScreen>
     );
 }
@@ -99,6 +168,16 @@ const CreateProfileScreen = styled.div`
 
 const SaveProfileBtn = styled(LightButton1)`
 `;
+
+
+
+
+const GenderChoice = styled.div`
+    margin-bottom: 10px;
+    align-items: center;
+
+`;
+
 
 
 
--- src/pages/profile/UploadPhotos.js
+++ src/pages/profile/UploadPhotos.js
@@ -3,6 +3,10 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import getHeaderToken from "../../utils/getHeaderToken";
 import { ThreeDots } from "react-loader-spinner";
+import { lightColor1 } from "../../assets/globalStyles/colors";
+import { LightButton1 } from "../../components/Button";
+import { IconLeftButton, IconRightButton } from "../../components/Icon";
+import { useNavigate } from "react-router-dom";
 
 const config = getHeaderToken();
 
@@ -10,6 +14,7 @@ export default function UploadPhotos() {
   const [pic, setPic] = useState();
   const [allPics, setAllPics] = useState([]);
   const [loadingPhoto, setLoadingPhoto] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     getAllPics();
@@ -29,16 +34,13 @@ export default function UploadPhotos() {
         alert("Não foi possível fazer o upload dessa foto, tente outra!");
     }
 
-    // await axios
-    //   .post("http://localhost:5000/uploadPhoto", formData, config)
-    //   .then(getAllPics())
-    //   .catch((error) => console.log(error.message));
   };
   const handleChange = (e) => {
     setPic(e.target.files[0]);
   };
 
   const getAllPics = async () => {
+    const config = getHeaderToken();
     const allPicsData = await axios.get("http://localhost:5000/getProfilePhotos", config);
     console.log(allPicsData.data);
 
@@ -48,10 +50,6 @@ export default function UploadPhotos() {
 
     setAllPics(allPicsData.data);
 
-    await axios
-      .get("http://localhost:5000/getProfilePhotos", config)
-      .then((res) => setAllPics(res.data))
-      .catch((error) => console.log(error.message));
   };
 
   const handleDelete = async (name) => {
@@ -63,11 +61,21 @@ export default function UploadPhotos() {
       .catch((error) => console.log(error.message));
   };
 
+  function validatePics(){
+    if(allPics.length < 1){
+      alert("É necessário pelo menos uma foto para continuar :(")
+      return;
+    }
+
+    navigate("/home");
+  }
+
   return (
     <UploadPhotosContainer className="app">
+      <h1>Escolha aqui as fotos mais lindas que tiver, clique em "Escolher arquivo" e após isso em "Upload" para subir as fotos, (uma de cada vez ok?)</h1>
       <form className="form" onSubmit={handleSubmit}>
-        <input type="file" onChange={handleChange} />
-        <button>{loadingPhoto === false ? "upload" : <ThreeDots color="white" />}</button>
+        <div><InputFile type="file" onChange={handleChange} /></div>
+        <UploadButton>{loadingPhoto === false ? "upload" : <ThreeDots color="white" />}</UploadButton>
       </form>
       <div className="imgsContainer">
         {allPics?.length > 0 ? 
@@ -75,59 +83,92 @@ export default function UploadPhotos() {
                         allPics.map((photo) => (
                           <div className="imgItem" key={photo.id}>
                             <img className="img" src={photo.photoUrl} alt="" />
-                            <button
+                            {/* <button
                               className="imgButton"
                               onClick={() => handleDelete(photo.id)}
                             >
                               Delete
-                            </button>
+                            </button> */}
                           </div>
-                        )) 
+                        ))
         :
-        <div>Not pics to show</div>
+        <div>Nenhuma foto ainda</div>
         }
       </div>
+        {allPics?.length > 0 ?
+          <>
+            <h1>Se já terminou, é só clicar abaixo e já vamos atrás do seu amor verdadeiro :)</h1>
+            <BtnComplete onClick={() => validatePics()}>Vamos lá</BtnComplete> 
+          </>
+        :
+        false  
+      }
     </UploadPhotosContainer>
   );
 }
 
+const BtnComplete = styled.div`
+  min-width: 100%;
+  min-height: 6vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid white;
+  width: 20px;
+  height: 20px;
+`
+
 const UploadPhotosContainer = styled.div`
-    .app {
-  margin: 20px 0;
-}
+
+    width:  100vw;
+    min-height: 100vh;
+    padding: 20px 20px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: ${lightColor1};
+    opacity: 0.95;
+
+    font-size: large;
+    font-family: 'Quicksand';
+    font-weight: 500;
+
+    h1{
+        margin-bottom: 50px;
+        font-size: larger;
+        font-weight: 500;
+        word-wrap: break-word;
+    }
 
 .form {
   display: flex;
-  justify-content: flex-end;
-  margin-right: 20px;
+  flex-direction: column;
 }
 
-.form > button {
-  border-radius: 10px;
-  background-color: purple;
-  border: none;
-  color: white;
-  width: 60px;
-  font-size: 0.8rem;
-}
 
 .imgsContainer {
+  width: 100%;
   margin: 20px;
   display: flex;
   justify-content: space-between;
   flex-wrap: wrap;
+  background-color: ${lightColor1};
 }
 
 .imgItem {
+  width: 100%;
   display: flex;
   flex-direction: column;
   margin-bottom: 15px;
   align-items: center;
+  border: 2px solid white;
 }
 
 .img {
   height: 300px;
-  width: 400px;
+  width: 100%;
+  object-fit: cover;
 }
 
 .imgButton {
@@ -139,4 +180,15 @@ const UploadPhotosContainer = styled.div`
   width: 60px;
   font-size: 0.8rem;
 } 
+`
+
+const UploadButton = styled(LightButton1)`
+  width: 100%;
+  margin-top: 10px;
+`
+
+const InputFile = styled.input`
+  width: 100%;
+  height: 20px;
+
 `
